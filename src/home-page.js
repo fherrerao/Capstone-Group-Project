442f1fb9f545e3377dd6ff6db80a946b48e6a0a1
MrRamoun <omartarekramoun@gmail.com>
2022-02-02 23:34:27 +0200

    Add popup window for each card

--- src/home-page.js
+++ src/home-page.js
@@ -1,5 +1,4 @@
 export default class Character {
-  static list = [];
 
   static url = 'https://api.tvmaze.com/search/shows?q=terror';
 
@@ -9,21 +8,6 @@ export default class Character {
     const response = await fetch(this.url);
     const data = await response.json();
 
-    const movieContainer = document.querySelector('.movie-container');
-
-    data.forEach((item) => {
-      const div = document.createElement('div');
-      div.classList.add('div-container');
-      div.innerHTML = `<img src="${item.show.image.medium}" alt="">
-      <div class="media flex main-space-between">
-        <li>${item.show.name}</li>
-        <div class="likes-container">
-          <box-icon name='heart'></box-icon>
-          <p>Likes</p>
-        </div>
-      </div>
-      <button class="button">Comments</button>`;
-      movieContainer.appendChild(div);
-    });
+    return data;
   };
 }
