a4cfbee7c99043a3776ab408b66c959a53fbad77
fherrerao <fherrera0206@gmail.com>
2022-02-02 10:45:24 -0500

    Fix getMovies function

--- src/home-page.js
+++ src/home-page.js
@@ -3,7 +3,7 @@ export default class Character {
   static url = "https://api.tvmaze.com/search/shows?q=terror";
 
   static list = [];
-  static getCharacter = async () => {
+  static getMovies = async () => {
     const response = await fetch(this.url);
     const data = await response.json();
 
