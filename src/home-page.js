22b19987b4607a3ee40323589a95ab20b5048ff9
fherrerao <fherrera0206@gmail.com>
2022-02-02 10:44:34 -0500

    Build getMovies function

---
+++ src/home-page.js
@@ -0,0 +1,27 @@
+export default class Character {
+  static list = [];
+  static url = "https://api.tvmaze.com/search/shows?q=terror";
+
+  static list = [];
+  static getCharacter = async () => {
+    const response = await fetch(this.url);
+    const data = await response.json();
+
+    const movieContainer = document.querySelector(".movie-container");
+    data.map((item) => {
+      const div = document.createElement("div");
+      div.classList.add("div-container");
+      div.innerHTML = `<img src="${item.show.image.medium}" alt="">
+      <div class="media flex main-space-between">
+        <li>${item.show.name}</li>
+        <div class="likes-container">
+          <box-icon name='heart'></box-icon>
+          <p>Likes</p>
+        </div>
+      </div>
+      <button class="button">Comments</button>`;
+
+      movieContainer.appendChild(div);
+    });
+  };
+}
