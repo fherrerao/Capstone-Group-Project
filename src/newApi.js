6d5f7cf5af102ed667fdbe44107b7b35f83594c9
fherrerao <fherrera0206@gmail.com>
2022-02-03 10:16:45 -0500

    Fix Api

--- src/newApi.js
+++ src/newApi.js
@@ -1,21 +1,22 @@
 export default class NewApi {
   static url =
-    'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/l0G9Hc9Wz9dyJkSVer74/likes';
+    "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/M6E1Pvb7mJsTEY4eMpjI/likes";
 
   static getLikes = async () => {
     const response = await fetch(this.url);
     const data = await response.json();
+    console.log(data);
     return data;
   };
 
   static setLike = async (id) => {
     const response = await fetch(this.url, {
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify({
         item_id: id,
       }),
       headers: {
-        'Content-type': 'application/JSON',
+        "Content-type": "application/JSON",
       },
     });
 
