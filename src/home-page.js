b092911197f3a7a79b1cbd44de445f3499abbd22
fherrerao <fherrera0206@gmail.com>
2022-02-02 21:18:08 -0500

    Update likes

--- src/home-page.js
+++ src/home-page.js
@@ -1,29 +1,51 @@
-export default class Character {
-  static list = [];
+import NewApi from "./newApi";
 
-  static url = 'https://api.tvmaze.com/search/shows?q=terror';
+export default class Movies {
+  static url = "https://api.tvmaze.com/search/shows?q=terror";
 
-  static list = [];
+  static updateLikes = () => {
+    NewApi.getLikes().then((data) => {
+      data.forEach((item) => {
+        const boxicon = document.getElementById(`${item.item_id}`);
+        boxicon.nextElementSibling.innerHTML = `${item.likes} likes`;
+      });
+    });
+  };
+
+  static setEventLikes = () => {
+    const likeIcon = document.querySelectorAll(".like-icon");
+    likeIcon.forEach((element, index) => {
+      element.addEventListener("click", () => {
+        console.log(parseInt(element.id));
+        NewApi.setLike(parseInt(element.id)).then(() => {
+          this.updateLikes();
+        });
+      });
+    });
+  };
 
   static getMovies = async () => {
     const response = await fetch(this.url);
     const data = await response.json();
+    console.log(data.length);
 
-    const movieContainer = document.querySelector('.movie-container');
+    const movieContainer = document.querySelector(".movie-container");
 
     data.forEach((item) => {
-      const div = document.createElement('div');
-      div.classList.add('div-container');
+      const div = document.createElement("div");
+      div.classList.add("div-container");
       div.innerHTML = `<img src="${item.show.image.medium}" alt="">
       <div class="media flex main-space-between">
         <li>${item.show.name}</li>
         <div class="likes-container">
-          <box-icon name='heart'></box-icon>
-          <p>Likes</p>
+          <box-icon id=${item.show.id} class="like-icon" name='heart'></box-icon>
+          <p>0 Likes</p>
         </div>
       </div>
       <button class="button">Comments</button>`;
       movieContainer.appendChild(div);
     });
+    this.setEventLikes();
+    this.updateLikes();
   };
 }
