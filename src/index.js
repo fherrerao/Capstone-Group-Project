695a95bfbcec81e081ba6a1bf88ee7a4c15f1051
Fernando Herrera <fherrera0206@gmail.com>
2022-02-04 13:53:31 -0500

    Fix about section

--- src/index.js
+++ src/index.js
@@ -5,19 +5,4 @@ import NewApi from "./newApi.js";
 Movies.getMovies();
 NewApi.getLikes();
 Movies.counterMovies();
-
-const moviesTab = document.querySelector(".movies-tab");
-const aboutTab = document.querySelector(".about-tab");
-const homePage = document.querySelector(".homepage");
-const aboutSection = document.querySelector(".about-section");
-
-aboutSection.classList.add("d-none");
-aboutTab.addEventListener("click", () => {
-  homePage.classList.add("d-none");
-  aboutSection.classList.remove("d-none");
-});
-
-moviesTab.addEventListener("click", () => {
-  homePage.classList.remove("d-none");
-  aboutSection.classList.add("d-none");
-});
+NewApi.aboutSection();
